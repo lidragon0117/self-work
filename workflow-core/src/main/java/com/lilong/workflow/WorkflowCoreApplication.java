e65b76de71a2697a8ed4960a65757603109494e7
lilong <1970404793@qq.com>
2024-11-30 11:29:04 +0800

    init

---
+++ workflow-core/src/main/java/com/lilong/workflow/WorkflowCoreApplication.java
@@ -0,0 +1,16 @@
+package com.lilong.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lilong
+ */
+@SpringBootApplication
+public class WorkflowCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkflowCoreApplication.class, args);
+    }
+
+}
---
+++ workflow-core/src/test/java/com/lilong/workflow/WorkflowCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lilong.workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
